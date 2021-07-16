03b6475fe9af4248c9527c0108ed3eeabc86f3d7
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-07-16 21:27:16 +0700

    update

---
+++ Exam/Exam/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Exam.Models;
+
+namespace Exam.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base ("Exam_WAD")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Exam.Models.Classroom> Classrooms { get; set; }
+
+        public System.Data.Entity.DbSet<Exam.Models.ExamSubject> ExamSubjects { get; set; }
+
+        public System.Data.Entity.DbSet<Exam.Models.Test> Tests { get; set; }
+
+        public System.Data.Entity.DbSet<Exam.Models.Falculty> Falculties { get; set; }
+    }
+}
---
+++ Exam/Exam/Models/Classroom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exam.Models
+{
+    public class Classroom
+    {
+        [Key]
+        public int ClassroomID { get; set; }
+        [Required(ErrorMessage = "Please enter the correct Classroom")]
+        public string Name { get; set; }
+    }
+}
